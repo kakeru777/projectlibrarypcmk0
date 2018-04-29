87feb0a17716220ed218ee21dd89cf758da3196a
feryadialoi@gmail.com <xiaomimi4lte>
2018-04-29 21:58:42 +0700

    edit file index

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 			 <div style="color: white;
 			padding: 15px 50px 5px 50px;
 			float: right;
-			font-size: 16px;"> [Last access : 31 May 2014] $time <a href="login.html" class="btn btn-danger square-btn-adjust">Logout</a> </div>
+			font-size: 16px;"> [Last access : 30 May 2014] $time <a href="login.html" class="btn btn-danger square-btn-adjust">Logout</a> </div>
       </nav>
            <!-- /. NAV TOP  -->
                 <nav class="navbar-default navbar-side" role="navigation">
