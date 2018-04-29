1a1590ada89d825891fe55f8731b0ce90bdc40f1
feryadialoi@gmail.com <xiaomimi4lte>
2018-04-29 21:56:55 +0700

    edit index

---
+++ index.php
@@ -0,0 +1,115 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Perpustakaan Pelita Cemerlang</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html">Perpustakaan</a>
+            </div>
+			 <div style="color: white;
+			padding: 15px 50px 5px 50px;
+			float: right;
+			font-size: 16px;"> [Last access : 31 May 2014] $time <a href="login.html" class="btn btn-danger square-btn-adjust">Logout</a> </div>
+      </nav>
+           <!-- /. NAV TOP  -->
+                <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+				<li class="text-center">
+                    <img src="assets/img/find_user.png" class="user-image img-responsive"/>
+					</li>
+
+
+
+                    <li>
+                        <a  href="?page=buku"><i class="fa fa-home fa-3x"></i> Home</a>
+                    </li>
+                      <li>
+                        <a  href="ui.html"><i class="fa fa-group fa-3x"></i>Keanggotaan</a>
+                    </li>
+                    <li>
+                        <a  href="tab-panel.html"><i class="fa fa-bookmark-o fa-3x"></i>Pinjam Buku</a>
+                    </li>
+						   <li  >
+                        <a  href="chart.html"><i class="fa fa-bookmark-o fa-3x"></i>Pengembalian Buku</a>
+                    </li>
+                      <li  >
+                        <a  href="table.html"><i class="fa fa-credit-card fa-3x"></i>Cetak Kartu Anggota</a>
+                    </li>
+                    <li  >
+                        <a  href="form.html"><i class="fa fa-book fa-3x"></i>Buku </a>
+                    </li>
+					<li  >
+                        <a   href="login.html"><i class="fa fa-bolt fa-3x"></i>Pengaturan</a>
+                    </li>
+                     <li  >
+                        <a   href="registeration.html"><i class="fa fa-info-circle fa-3x"></i>About</a>
+                    </li>
+                      </li>
+                </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <?php
+					 $page = $_GET['page'];
+				//	 $aksi = $_GET['aksi'];
+					 if($page == "buku"){
+						 if($aksi == ""){
+							 include "pages/book/buku.php";
+						 }
+					 }
+					 ?>
+
+
+
+
+                    </div>
+                </div>
+                 <!-- /. ROW  -->
+                 <hr />
+
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+      <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
